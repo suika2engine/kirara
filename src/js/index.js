2292e3f246d3be26fec290ff831bc2c2bad5669f
Keiichi TABATA <tabata.keiichi@gmail.com>
2023-01-11 18:49:44 +0900

    Added pallet

--- src/js/index.js
+++ src/js/index.js
@@ -1,145 +1,167 @@
 window.addEventListener('load', async () => {
-    const txt = await window.api.getTxtList();
-    const bg = await window.api.getBgList();
-    const ch = await window.api.getChList();
-    const bgm = await window.api.getBgmList();
-    const se = await window.api.getSeList();
+    //
+    // パレットの要素にイベントリスナを追加する
+    //
+    Array.from(document.getElementById("palette").childNodes).forEach(function (a) {
+        a.addEventListener("click", () => {
+            Array.from(document.getElementById("palette").childNodes).forEach(function (b) {
+                if(b.className == "left-tab-list-item" || b.className == "left-tab-list-item-sel") {
+                    if(b === event.srcElement) {
+                        b.className = "left-tab-list-item-sel";
+                    } else {
+                        b.className = "left-tab-list-item";
+                    }
+                }
+            });
+            document.getElementById("thumbnail-picture").src = "";
+        });
+    });
 
+    //
+    // txtフォルダの中身を取得してリスト要素を追加する
+    //
+    const txt = await window.api.getTxtList();
     txt.forEach(function(file) {
         var elem = document.createElement('li');
         elem.textContent = file;
         elem.draggable = "true";
         elem.className = "left-tab-list-item";
-        elem.addEventListener("click", onClickTxt);
+        elem.addEventListener("click", () => {
+            Array.from(document.getElementById("txt-list").childNodes).forEach(function (a) {
+                if(a.className == "left-tab-list-item" || a.className == "left-tab-list-item-sel") {
+                    if(a === event.srcElement) {
+                        a.className = "left-tab-list-item-sel";
+                    } else {
+                        a.className = "left-tab-list-item";
+                    }
+                }
+            });
+            document.getElementById("thumbnail-picture").src = "";
+        });
         document.getElementById("txt-list").appendChild(elem);
     });
+
+    //
+    // bgフォルダの中身を取得してリスト要素を追加する
+    //
+    const bg = await window.api.getBgList();
     bg.forEach(function(file) {
         var elem = document.createElement('li');
         elem.textContent = file;
         elem.draggable = "true";
         elem.className = "left-tab-list-item";
-        elem.addEventListener("click", onClickBg);
+        elem.addEventListener("click", async () => {
+            Array.from(document.getElementById("bg-list").childNodes).forEach(function (a) {
+                if(a.className == "left-tab-list-item" || a.className == "left-tab-list-item-sel") {
+                    if(a === event.srcElement) {
+                        a.className = "left-tab-list-item-sel";
+                    } else {
+                        a.className = "left-tab-list-item";
+                    }
+                }
+            });
+            document.getElementById("thumbnail-picture").src = await window.api.getBaseUrl() + "bg/" + file;
+        });
         document.getElementById("bg-list").appendChild(elem);
     });
+
+    //
+    // chフォルダの中身を取得してリスト要素を追加する
+    //
+    const ch = await window.api.getChList();
     ch.forEach(function(file) {
         var elem = document.createElement('li');
         elem.textContent = file;
         elem.draggable = "true";
         elem.className = "left-tab-list-item";
-        elem.addEventListener("click", onClickCh);
+        elem.addEventListener("click", async () => {
+            Array.from(document.getElementById("ch-list").childNodes).forEach(function (a) {
+                if(a.className == "left-tab-list-item" || a.className == "left-tab-list-item-sel") {
+                    if(a === event.srcElement) {
+                        a.className = "left-tab-list-item-sel";
+                    } else {
+                        a.className = "left-tab-list-item";
+                    }
+                }
+            });
+            document.getElementById("thumbnail-picture").src = await window.api.getBaseUrl() + "ch/" + file;
+        });
         document.getElementById("ch-list").appendChild(elem);
     });
+
+    //
+    // bgmフォルダの中身を取得してリスト要素を追加する
+    //
+    const bgm = await window.api.getBgmList();
     bgm.forEach(function(file) {
         var elem = document.createElement('li');
         elem.textContent = file;
         elem.draggable = "true";
         elem.className = "left-tab-list-item";
-        elem.addEventListener("click", onClickBgm);
+        elem.addEventListener("click", () => {
+            Array.from(document.getElementById("bgm-list").childNodes).forEach(function (a) {
+                if(a.className == "left-tab-list-item" || a.className == "left-tab-list-item-sel") {
+                    if(a === event.srcElement) {
+                        a.className = "left-tab-list-item-sel";
+                    } else {
+                        a.className = "left-tab-list-item";
+                    }
+                }
+            });
+            document.getElementById("thumbnail-picture").src = "";
+        });
         document.getElementById("bgm-list").appendChild(elem);
     });
+
+    //
+    // seフォルダの中身を取得してリスト要素を追加する
+    //
+    const se = await window.api.getSeList();
     se.forEach(function(file) {
         var elem = document.createElement('li');
         elem.textContent = file;
         elem.draggable = "true";
         elem.className = "left-tab-list-item";
-        elem.addEventListener("click", onClickSe);
+        elem.addEventListener("click", () => {
+            Array.from(document.getElementById("se-list").childNodes).forEach(function (a) {
+                if(a.className == "left-tab-list-item" || a.className == "left-tab-list-item-sel") {
+                    if(a === event.srcElement) {
+                        a.className = "left-tab-list-item-sel";
+                    } else {
+                        a.className = "left-tab-list-item";
+                    }
+                }
+            });
+            document.getElementById("thumbnail-picture").src = "";
+        });
         document.getElementById("se-list").appendChild(elem);
     });
+
+    //
+    // メインリストビューのドラッグアンドドロップ動作を設定する
+    //
+    document.querySelectorAll('.drag-list li').forEach (elm => {
+        elm.ondragstart = function () {
+            event.dataTransfer.setData('text/plain', event.target.id);
+            return true;
+        };
+        elm.ondragover = function () {
+            if(event.srcElement == document.getElementById("start-item")) {
+                return true;
+            }
+            event.preventDefault();
+            this.style.borderTop = '2px solid blue';
+            return false;
+        };
+        elm.ondragleave = function () {
+            this.style.borderTop = '';
+        };
+        elm.ondrop = function () {
+            event.preventDefault();
+            let id = event.dataTransfer.getData('text/plain');
+            let elm_drag = document.getElementById(id);
+            this.parentNode.insertBefore(elm_drag, this);
+            this.style.borderTop = '';
+        };
+    });
 })
-
-function onClickTxt() {
-    Array.from(document.getElementById("txt-list").childNodes).forEach(function (a) {
-        if(a.className == "left-tab-list-item" || a.className == "left-tab-list-item-sel") {
-            if(a === event.srcElement) {
-                a.className = "left-tab-list-item-sel";
-            } else {
-                a.className = "left-tab-list-item";
-            }
-        }
-    });
-    loadPicture(null, null);
-}
-
-function onClickBg() {
-    Array.from(document.getElementById("bg-list").childNodes).forEach(function (a) {
-        if(a.className == "left-tab-list-item" || a.className == "left-tab-list-item-sel") {
-            if(a === event.srcElement) {
-                a.className = "left-tab-list-item-sel";
-            } else {
-                a.className = "left-tab-list-item";
-            }
-        }
-    });
-    loadPicture("bg", event.srcElement.textContent);
-}
-
-function onClickCh() {
-    Array.from(document.getElementById("ch-list").childNodes).forEach(function (a) {
-        if(a.className == "left-tab-list-item" || a.className == "left-tab-list-item-sel") {
-            if(a === event.srcElement) {
-                a.className = "left-tab-list-item-sel";
-            } else {
-                a.className = "left-tab-list-item";
-            }
-        }
-    });
-    loadPicture("ch", event.srcElement.textContent);
-}
-
-function onClickBgm(elem) {
-    Array.from(document.getElementById("bgm-list").childNodes).forEach(function (a) {
-        if(a.className == "left-tab-list-item" || a.className == "left-tab-list-item-sel") {
-            if(a === event.srcElement) {
-                a.className = "left-tab-list-item-sel";
-            } else {
-                a.className = "left-tab-list-item";
-            }
-        }
-    });
-    loadPicture(null, null);
-}
-
-function onClickSe(elem) {
-    Array.from(document.getElementById("se-list").childNodes).forEach(function (a) {
-        if(a.className == "left-tab-list-item" || a.className == "left-tab-list-item-sel") {
-            if(a === event.srcElement) {
-                a.className = "left-tab-list-item-sel";
-            } else {
-                a.className = "left-tab-list-item";
-            }
-        }
-    });
-    loadPicture(null, null);
-}
-
-async function loadPicture(dir, file) {
-    var url = "";
-    if(dir != null && file != null) {
-        url = await window.api.getBaseUrl() + dir + "/" + file;
-    }
-    document.getElementById("thumbnail-picture").src = url;
-}
-
-
-
-
-
-document.querySelectorAll('.drag-list li').forEach (elm => {
-    elm.ondragstart = function () {
-        event.dataTransfer.setData('text/plain', event.target.id);
-    };
-    elm.ondragover = function () {
-        event.preventDefault();
-        this.style.borderTop = '2px solid blue';
-    };
-    elm.ondragleave = function () {
-        this.style.borderTop = '';
-    };
-    elm.ondrop = function () {
-        event.preventDefault();
-        let id = event.dataTransfer.getData('text/plain');
-        let elm_drag = document.getElementById(id);
-        this.parentNode.insertBefore(elm_drag, this);
-        this.style.borderTop = '';
-    };
-});

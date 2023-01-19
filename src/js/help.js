d42d6bf20eda31d6d28baad9825c8011fd111d4e
KyouRobert <kyoubrayden@gmail.com>
2023-01-20 00:34:25 +1300

    Created help.js file

---
+++ src/js/help.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", async() => {
+    document.getElementById("discord-link").addEventListener("click", () => {
+        window.api.openURL("https://discord.gg/ZmvXxE8GFg");
+    });
+})
+
+window.addEventListener("load", async() => {
+    document.getElementById("wiki-s2s").addEventListener("click", () => {
+        window.api.openURL("https://github.com/suika2engine/suika2/wiki/3.-Suika2Script-and-Commands");
+    });
+})

1dd02380ab107387766a2547ab79d2764514597f
KyouRobert <kyoubrayden@gmail.com>
2023-01-21 23:00:43 +1300

    Added 'pages' style, script, and general code clean-up

--- src/js/help.js
+++ src/js/help.js
@@ -1,4 +1,12 @@
+//
+// リンク | Links
+//
+
 window.addEventListener("load", async() => {
+    document.getElementById("web-doc").addEventListener("click", () => {
+      window.api.openURL("https://github.com/suika2engine/suika2/wiki");
+    });
+
     document.getElementById("discord-link").addEventListener("click", () => {
         window.api.openURL("https://discord.gg/ZmvXxE8GFg");
     });
@@ -7,3 +15,10 @@ window.addEventListener("load", async() => {
         window.api.openURL("https://github.com/suika2engine/suika2/wiki/3.-Suika2Script-and-Commands");
     });
 })
+
+//
+// ドキュメント | Document
+//
+
+// Jump to 'welcome' (Documentation Home) page when opened or refreshed.
+document.location.hash = "#welcome";

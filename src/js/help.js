9d0066a16c8764b90983a27845722c864fe6f207
KyouRobert <kyoubrayden@gmail.com>
2023-01-26 22:37:28 +1300

    Create unified style for all main pages, including navigation. Fixed several issues with spacing and units. returned boot screen to single-layer (part 2/2). Fixed help links opening within application, they now open correctly (through system web browser).

--- src/js/help.js
+++ src/js/help.js
@@ -3,16 +3,8 @@
 //
 
 window.addEventListener("load", async() => {
-    document.getElementById("web-doc").addEventListener("click", () => {
+    document.getElementById("web-docs").addEventListener("click", () => {
       window.api.openURL("https://github.com/suika2engine/suika2/wiki");
-    });
-
-    document.getElementById("discord-link").addEventListener("click", () => {
-        window.api.openURL("https://discord.gg/ZmvXxE8GFg");
-    });
-
-    document.getElementById("wiki-s2s").addEventListener("click", () => {
-        window.api.openURL("https://github.com/suika2engine/suika2/wiki/3.-Suika2Script-and-Commands");
     });
 })
 
@@ -20,5 +12,5 @@ window.addEventListener("load", async() => {
 // ドキュメント | Document
 //
 
-// Jump to 'welcome' (Documentation Home) page when opened or refreshed.
-document.location.hash = "#welcome";
+// Jump to 'pagew' (Home) page when opened or refreshed.
+document.location.hash = "#pagew";
